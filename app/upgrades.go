969dbccfb3b49d58ff1e1b47e254211d3bcab2af
Raul Bernal <bernal.raul@gmail.com>
2024-05-29 08:59:59 +0200

    Upgrade handler for stickyfingers

---
+++ app/upgrades.go
@@ -0,0 +1,129 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	storetypes "cosmossdk.io/store/types"
+	circuittypes "cosmossdk.io/x/circuit/types"
+	ibcfeetypes "github.com/cosmos/ibc-go/v8/modules/apps/29-fee/types"
+
+	nft "cosmossdk.io/x/nft"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	// WASM upgrade/addition
+	v2 "github.com/CosmWasm/wasmd/x/wasm/migrations/v2"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+// RegisterUpgradeHandlers registers upgrade handlers.
+
+func (app App) RegisterUpgradeHandlers() {
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	app.StickyFingers(upgradeInfo)
+}
+func (app *App) StickyFingers(_ upgradetypes.Plan) {
+	planName := "stickyfingers"
+	app.UpgradeKeeper.SetUpgradeHandler(
+		planName,
+		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			app.Logger().Info("Cosmos-SDK v0.50.x is here...")
+			// Print the modules with their respective ver.
+			for moduleName, version := range fromVM {
+				app.Logger().Info(fmt.Sprintf("Module: %s, Version: %d", moduleName, version))
+
+			}
+			// WASM
+			setupLegacyKeyTables(&app.ParamsKeeper)
+			app.GetStoreKeys()
+
+			versionMap, err := app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+			if err != nil {
+				return nil, err
+			}
+			app.Logger().Info(fmt.Sprintf("post migrate version map: %v", versionMap))
+			// return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+			return versionMap, err
+		},
+	)
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(fmt.Sprintf("failed to read upgrade info from disk %s", err))
+	}
+
+	if upgradeInfo.Name == planName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{
+				circuittypes.ModuleName, // commented at v0.50>v0.50 uncomment for v0.47>v0.50
+				ibcfeetypes.ModuleName,  // commented at v0.50>v0.50 uncomment for v0.47>v0.50
+				nft.ModuleName,          // commented at v0.50>v0.50 uncomment for v0.47>v0.50
+				wasmtypes.ModuleName,
+			},
+			Deleted: []string{
+				"burn",
+				"bcna",
+			},
+		}
+
+		// configure store loader that checks if version == upgradeHeight and applies store upgrades
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+
+}
+
+func setupLegacyKeyTables(k *paramskeeper.Keeper) {
+	for _, subspace := range k.GetSubspaces() {
+		subspace := subspace
+
+		var keyTable paramstypes.KeyTable
+		switch subspace.Name() {
+		case authtypes.ModuleName:
+			keyTable = authtypes.ParamKeyTable() //nolint:staticcheck
+		case banktypes.ModuleName:
+			keyTable = banktypes.ParamKeyTable() //nolint:staticcheck
+		case stakingtypes.ModuleName:
+			keyTable = stakingtypes.ParamKeyTable() //nolint:staticcheck
+		case minttypes.ModuleName:
+			keyTable = minttypes.ParamKeyTable() //nolint:staticcheck
+		case distrtypes.ModuleName:
+			keyTable = distrtypes.ParamKeyTable() //nolint:staticcheck
+		case slashingtypes.ModuleName:
+			keyTable = slashingtypes.ParamKeyTable() //nolint:staticcheck
+		case govtypes.ModuleName:
+			keyTable = govv1.ParamKeyTable() //nolint:staticcheck
+		case crisistypes.ModuleName:
+			keyTable = crisistypes.ParamKeyTable() //nolint:staticcheck
+			// wasm
+		case wasmtypes.ModuleName:
+			keyTable = v2.ParamKeyTable() //nolint:staticcheck
+		default:
+			continue
+		}
+
+		if !subspace.HasKeyTable() {
+			subspace.WithKeyTable(keyTable)
+		}
+	}
+	// sdk 47
+	k.Subspace(baseapp.Paramspace).
+		WithKeyTable(paramstypes.ConsensusParamsKeyTable())
+}
