37dc0f5038211907b885933181d76440df4bbc86
Raul Bernal <bernal.raul@gmail.com>
2024-05-29 09:15:55 +0200

    app.go RegisterUpgradeHandlers()

--- app/app.go
+++ app/app.go
@@ -337,6 +337,8 @@ func New(
 
 	app.ModuleManager.RegisterInvariants(app.CrisisKeeper)
 
+	app.RegisterUpgradeHandlers()
+
 	// create the simulation manager and define the order of the modules for deterministic simulations
 	//
 	// NOTE: this is not required apps that don't use the simulator for fuzz testing transactions
